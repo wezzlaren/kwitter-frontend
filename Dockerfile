7a5f7003e1fc6ed9736978a8977850594c5c1ebb
wezzlaren <wesley2810@hotmail.com>
2020-05-28 19:22:28 +0200

    add build

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
 FROM nginx:1.17
-COPY build/* /usr/share/nginx/html
+COPY build/ /usr/share/nginx/html
---
+++ build/precache-manifest.d761f689807338204b572ab7d38251a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2146713a466eadcb43249289af6e4744",
+    "url": "/index.html"
+  },
+  {
+    "revision": "766a994cd0d0f103a60a",
+    "url": "/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "8c3b9d1f7fdd61804b8d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "766a994cd0d0f103a60a",
+    "url": "/static/js/2.c22f4959.chunk.js"
+  },
+  {
+    "revision": "f925ff808c907f1e05af05a547934faf",
+    "url": "/static/js/2.c22f4959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c3b9d1f7fdd61804b8d",
+    "url": "/static/js/main.e5d9149b.chunk.js"
+  },
+  {
+    "revision": "ab0dca202c6b1f6c7a89",
+    "url": "/static/js/runtime-main.cd623502.js"
+  }
+]);
